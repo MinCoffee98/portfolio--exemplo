630a05911bda4ee36041accb860039f19d1442c0
MinCoffee98 <isa98vih@gmail.com>
2025-03-13 14:00:56 -0300

    Portfolio exemplo

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('contatoForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    alert(`Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`);
+    
+    // Aqui você pode adicionar a lógica para enviar os dados para um servidor, se necessário.
+
+    // Limpar o formulário
+    document.getElementById('contatoForm').reset();
+});
+
+document.getElementById('whatsappBtn').addEventListener('click', function() {
+    window.open('https://wa.me/11959870300', '_blank'); 
+});
+
+//document.getElementById('instagramBtn').addEventListener('click', function() {
+    //window.open('https://www.instagram.com/b_vitoria98', '_blank'); 
+//});
